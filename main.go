51771e79e0b98eb89d1b82474b63537d3a9dd868
musishere <mustafabukhari333@gmail.com>
2026-01-28 23:07:16 +0500

    feat: add custom logger to server

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/musishere/microservices/handlers"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			rw.Write([]byte("Oops"))
-			return
-		}
-
-		fmt.Fprintf(rw, "Hello from request writer %s", d)
-	})
+	http.HandleFunc("/", nil)
 
 	http.ListenAndServe(":9090", nil)
 }
