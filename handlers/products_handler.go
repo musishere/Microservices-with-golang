f422539dec9bd2238d2d225adb288fc9b86d00f7
musishere <mustafabukhari333@gmail.com>
2026-01-31 01:39:39 +0500

    Update product api

--- handlers/products_handler.go
+++ handlers/products_handler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/musishere/microservices/product-api/data"
 )
@@ -26,8 +28,24 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPut {
+		p.l.Println("Put Request")
 		// expect the id in the URI
-		p := r.URL.Path
+		reg := regexp.MustCompile(`/([0-9]+)`) //regex
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, _ := strconv.Atoi(idString)
+		p.l.Println("Got id: ", id)
+
+		p.updateProducts(id, rw, r)
 	}
 
 	// catch all
@@ -59,4 +77,25 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Prod: %#v", prod)
 
 	data.AddProduct(prod)
+}
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
